5778be4bb65e33749867bc786f53ab27ea1f9507
Xi Xia <triplextension@gmail.com>
2018-04-21 19:29:55 -0700

    server.js and webpack production config initiated

---
+++ webpack.production.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+//module.exports details go here
