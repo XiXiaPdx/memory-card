022f84d6a549d4443455733b449672c628169e02
Xi Xia <triplextension@gmail.com>
2018-04-21 19:25:05 -0700

    inital commit. webpack dev server runs and index.html displays

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  entry: './scripts/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+            {from:'images',to:'images'}
+        ]),
+    new HtmlWebpackPlugin({
+      title:'Portfolio',
+      template: './index.html',
+      inject: 'body',
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        useShortDoctype: true
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          },
+          {
+            loader:'image-webpack-loader',
+            options: {
+
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
