58e87a3f0985265aa380c03c1868f38378868c06
Xi Xia <triplextension@gmail.com>
2018-04-22 20:38:41 -0700

    added find card function to js

---
+++ README.md
@@ -0,0 +1,30 @@
+Use Css grid and create following areas:
+- Top score area tracks moves.
+- Top area has restart game button.
+- top has stars.  Too many moves, stars start disappearing.
+
+Different screen when winning game, restart button on screen, shows # of moves.
+
+- create a "table", give it box shadow.
+- table has 4 x 4 grid system with gutters. use grid area names "0 to 15".
+- each grid has a div (class) and single img element.
+- img element has rounded corners, holds svg
+
+Test:
+
+create a grid 2 x 2. done.
+create grid area names. done.
+create an img. done.
+move the img around with javascript? Yes, tested.
+
+it works, grid area must be strings, not numbers.  Associate 0-15 array with alphabet a to (whatever is 15).
+
+- In array is integer 1-8, switch statement 1-8 for different images.
+
+Test:
+
+switch statement flow 1-8 show different image? Yes, it works. 
+
+- Whatever card is at 0 goes to A. And so on until 15.
+
+Test:
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,18 @@
 import '../styles/styles.css';
 import xMark from '../images/xMark.svg';
+import noBolt from '../images/noBolt.svg';
+import monkey from '../images/monkey.svg';
+import head from '../images/head.svg';
+import five from '../images/five.svg';
+import eyes from '../images/eyes.svg';
+import circleX from '../images/circleX.svg';
+import body from '../images/body.svg';
 
 let xMarkImage = document.querySelector('#xMark');
 xMarkImage.src = xMark;
+
+xMarkImage.style.gridArea = "z";
+
 
 //8 pairs of cards randomized.  Take numbers 1 to 8, place randomly into array, twice.
 
@@ -27,4 +37,36 @@ function shuffleCards () {
   console.log(allCards);
 }
 
+function findCard(cardNumber){
+  let selectedCard;
+  switch (cardNumber){
+    case 1:
+      selectedCard = xMark;
+      break;
+    case 2:
+      selectedCard = noBolt;
+      break;
+    case 3:
+      selectedCard = monkey;
+      break;
+    case 4:
+      selectedCard = head;
+      break;
+    case 5:
+      selectedCard = five;
+      break;
+    case 6:
+      selectedCard = eyes;
+      break;
+    case 7:
+      selectedCard = circleX;
+      break;
+    case 8:
+      selectedCard = body;
+      break;
+  }
+  return selectedCard;
+}
+
 shuffleCards();
+xMarkImage.src = findCard(allCards[2]);
